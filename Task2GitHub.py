fa2835bf2d6120facec1e25ca88ea6f48c4c3197
KateSkoby <katesko@yandex.ru>
2023-03-01 11:43:05 +0300

    Design as a function

--- Task2GitHub.py
+++ Task2GitHub.py
@@ -1,6 +1,9 @@
 from random import randint
 
 array = []
-array = [randint(5, 1900) for i in range(0, 29)]
-print(array)
 
+def createArray(number):
+    array = [randint(5, number*100) for i in range(0, number+10)]
+    print(array)
+
+createArray(19)
